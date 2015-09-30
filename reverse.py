eecd092878ef24953a91641ef0860b6cc3274233
moakq <muhammed.othman@students.makeschool.com>
2015-09-29 21:48:05 -0700

    {reverse command line arguments}

---
+++ reverse.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def reverse_arguments(arguments):
+    reversed_arguments = arguments[::-1]
+    for i in range(len(reversed_arguments)):
+        reversed_arguments[i] = reversed_arguments[i][::-1]
+    return reversed_arguments
+
+if __name__ == '__main__':
+    reversed = reverse_arguments(sys.argv[1:])
+    print(reversed)
